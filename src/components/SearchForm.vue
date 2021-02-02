378ba998fe535ec66ec70438d7b2c6baeb966302
wbtaro <m.ryotaro.325@gmail.com>
2021-02-03 04:05:53 +0900

    First commit

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <form>
+    <!-- <input type="text" v-model="searchCondition.station" @input="reflectSearchCondition"> -->
+    <input type="text" v-model="searchCondition.station">
+    <span>駅から</span>
+    <!-- <input type="text" v-model="searchCondition.upperMinute" @input="reflectSearchCondition"> -->
+    <input type="text" v-model="searchCondition.upperMinute">
+    <span>分以内</span>
+  </form>
+  </div>
+</template>
+
+<script>
+import {
+defineComponent,
+//   toRefs,
+//   computed,
+} from '@vue/composition-api';
+
+export default defineComponent({
+  props: {
+    searchCondition: Object,
+    index: Number
+  },
+  methods: {
+    // reflectSearchCondition: () => {
+    //   context.emit("inputConditions")
+    // }
+  }
+})
+</script>
---
+++ src/views/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <searchForm
+      is="search-form"
+      v-for="(searchCondition, index) in searchConditions"
+      :key="index"
+      :index="index"
+      :searchCondition="searchCondition"
+     >
+     </searchForm>
+  </div>
+</template>
+
+<script>
+import {
+  defineComponent,
+} from '@vue/composition-api'
+import SearchForm from "@/components/SearchForm.vue"
+
+class SearchCondition {
+  constructor(){
+    this.station = '';
+    this.upperMinute = '';
+  }
+}
+
+export default defineComponent({
+  components: {
+    'search-form': SearchForm
+  },
+  data(){
+    return {
+      searchConditions: [new SearchCondition()],
+    }
+  },
+})
+</script>
