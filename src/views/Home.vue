6e2e969261e02935e786144b2d01a7e7211695f5
wbtaro <m.ryotaro.325@gmail.com>
2021-02-23 21:21:20 +0900

    Add randing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="container"></div>
+  <div>
+    <div class="container-fluid bg-light">
+      <div class="mx-auto py-5 justify-content-center text-center">
+        <h2 class="display-4 mb-4 font-weight-bold">Trange</h2>
+        <p class="mb-2">Trangeは起点駅から指定した時間内に到着できる駅を検索できるサービスです</p>
+        <p>複数の条件を指定することもできます</p>
+        <p class="my-5">
+          <a href="/search">
+            <button class="btn btn-primary btn-lg">検索する</button>
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
